194b189db444423cf90e92af24dcbe4fedc7e566
Sabrina Hassan Moon <164791791+scorpiosh@users.noreply.github.com>
2025-08-11 21:58:40 -0500

    Update Count_Clock.v
    comment added

--- Count_Clock.v
+++ Count_Clock.v
@@ -71,9 +71,3 @@ module top_module(
     
 
 endmodule
-//Create a set of counters suitable for use as a 12-hour clock (with am/pm indicator). 
-//Your counters are clocked by a fast-running clk, with a pulse on ena whenever your clock should increment (i.e., once per second).
-
-//reset resets the clock to 12:00 AM. pm is 0 for AM and 1 for PM. hh, mm, and ss are two 
-//BCD (Binary-Coded Decimal) digits each for hours (01-12), minutes (00-59), and seconds (00-59). Reset has higher priority than enable,
-// and can occur even when not enabled.
